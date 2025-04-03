fffce568bcdb36cd2448596f7ef2aa597ee3d767
subrayahegde <68839512+subrayahegde@users.noreply.github.com>
2025-04-03 09:35:28 +0530

    Add files via upload

---
+++ next-mongo/pages/_document.js
@@ -0,0 +1,24 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default Document;
+
+function Document() {
+    return (
+        <Html lang="en">
+            <Head>
+                {/* eslint-disable-next-line @next/next/no-css-tags */}
+                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" />
+            </Head>
+
+            <body>
+                <Main />
+                <NextScript />
+
+                {/* credits */}
+                <div className="text-center mt-4">
+                  
+                </div>
+            </body>
+        </Html>
+    );
+}
